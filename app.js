46ce9c9891a9c27b42dc7514cb46e764673715eb
IvanildoBorges <ivanildo.rap@gmail.com>
2024-02-05 16:26:10 -0300

    Adicionado entrada de nome de jogador e patentes de nivel

--- app.js
+++ app.js
@@ -2,6 +2,47 @@ let listaDeNumerosSorteados = [];
 let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+let nickname = '';
+let patente = '';
+let pontosDeExperiencia = 0;
+
+function geraGamePlay() {
+    const containerHTML = document.querySelector('.container');
+    const containerGame = `
+        <div class="container__conteudo">
+            <div class="container__informacoes">
+                <div class="container__texto">
+                    <div class="container__xp">
+                        <span></span>
+                    </div>
+                    <h1>Adivinhe o <span class="container__texto-azul">numero secreto</span></h1>
+                    <p class="texto__paragrafo">Escolha um número entre 1 a 100</p>
+                </div>
+                <input type="number" min="1" max="10" class="container__input">
+                <div class="chute container__botoes">
+                    <button onclick="verificarChute()" class="container__botao">Chutar</button>
+                    <button onclick="reiniciarJogo()" id="reiniciar" class="container__botao" disabled>Novo jogo</button>
+                </div>
+            </div>
+            <img src="./img/ia.png" alt="Uma pessoa olhando para a esquerda" class="container__imagem-pessoa" />
+        </div>
+    `
+
+    containerHTML.innerHTML = containerGame;
+}
+
+function salvarNome() {
+    const inputNomeDoJogador = document.querySelector("input").value;
+
+    if (inputNomeDoJogador.trim() != null && inputNomeDoJogador.trim() != '') {
+        nickname = inputNomeDoJogador;
+    } else {
+        nickname = 'player123';
+    }
+
+    geraGamePlay()
+    exibirMensagemInicial();
+};
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -10,11 +51,47 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('.container__texto h1', 'Jogo do número secreto');
+    exibirTextoNaTela('.container__texto p', `Escolha um número entre 1 e ${numeroLimite}`);
+    aumentaXP()
 }
 
-exibirMensagemInicial();
+function aumentaXP() {
+    // Classificador de nível
+    let xpSpan = document.querySelector('span');
+
+    if (pontosDeExperiencia <= 1000) {
+        patente = 'Ferro'
+    } else if (pontosDeExperiencia > 1000 && pontosDeExperiencia <= 2000) {
+        patente = 'Bronze'
+    } else if (pontosDeExperiencia > 2000 && pontosDeExperiencia <= 5000) {
+        patente = 'Prata'
+    } else if (pontosDeExperiencia > 5000 && pontosDeExperiencia <= 7000) {
+        patente = 'Ouro'
+    } else if (pontosDeExperiencia > 7000 && pontosDeExperiencia <= 8000) {
+        patente = 'Platina'
+    } else if (pontosDeExperiencia > 8000 && pontosDeExperiencia <= 9000) {
+        patente = 'Ascedente'
+    } else if (pontosDeExperiencia > 9000 && pontosDeExperiencia <= 10000) {
+        patente = 'Imortal'
+    } else if (pontosDeExperiencia > 10000 && pontosDeExperiencia <= 11000){
+        patente = 'Radiante I'
+    } else if (pontosDeExperiencia > 11000 && pontosDeExperiencia <= 12000){
+        patente = 'Radiante II'
+    } else if (pontosDeExperiencia > 12000 && pontosDeExperiencia <= 13000){
+        patente = 'Radiante III'
+    } else if (pontosDeExperiencia > 13000 && pontosDeExperiencia <= 14000){
+        patente = 'Radiante IV'
+    } else if (pontosDeExperiencia > 14000){
+        patente = 'Radiante V'
+    }
+
+    xpSpan.innerHTML = `
+        Jogador: ${nickname} <br>
+        <span class="${patente.toLocaleLowerCase()}">Patente: ${patente}</span><br>
+        Pontos de experiência: ${pontosDeExperiencia}
+    `;
+}
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -26,15 +103,19 @@ function verificarChute() {
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
         document.querySelector('button').setAttribute('disabled', true);
+        pontosDeExperiencia += 1500;
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');
         } else {
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
+        pontosDeExperiencia -= 50;
         tentativas++;
         limparCampo();
     }
+
+    aumentaXP();
 }
 
 function gerarNumeroAleatorio() {
@@ -48,7 +129,6 @@ function gerarNumeroAleatorio() {
         return gerarNumeroAleatorio();
     } else {
         listaDeNumerosSorteados.push(numeroEscolhido);
-        console.log(listaDeNumerosSorteados)
         return numeroEscolhido;
     }
 }
@@ -66,10 +146,3 @@ function reiniciarJogo() {
     document.getElementById('reiniciar').setAttribute('disabled', true)
     document.querySelector('button').removeAttribute('disabled');
 }
-
-
-
-
-
-
-
